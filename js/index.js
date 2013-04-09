4f3f5bc2c2a73ae74cca7303f7065f53f0467681
Ian Yates <ian@snaptin.com>
2013-04-09 14:43:35 +0200

    First Commit

---
+++ js/index.js
@@ -0,0 +1,83 @@
+$("#signup").validate({
+  success : function(label){
+    label.addClass("valid").text("✓");
+  },
+  error : function(e){
+//     console.log(e);
+  },
+  onsubmit:false,
+  rules: {
+    phone: {
+      required: true,
+      phoneUS: true
+    }
+  }
+});
+
+$("body").on("keyup", "form", function(e){
+  if (e.which == 13){
+    if ($("#next").is(":visible") && $("fieldset.current").find("input, textarea").valid() ){
+      e.preventDefault();
+      nextSection();
+      return false;
+    }
+  }
+});
+
+
+$("#next").on("click", function(e){
+  console.log(e.target);
+  nextSection();
+});
+
+
+$("form").on("submit", function(e){
+  if ($("#next").is(":visible") || $("fieldset.current").index() < 3){
+    e.preventDefault();
+    return false;
+  }
+});
+  
+  
+  
+function nextSection(){
+  var i = $("fieldset.current").index();
+  if (i < 3){
+    $("li").eq(i+1).addClass("active");
+    goToSection(i+1);
+  }
+}
+
+$("li").on("click", function(e){
+  var i = $(this).index();
+  if ($(this).hasClass("active")){
+    goToSection(i);
+  } else {
+    alert("Please complete previous sections first.");
+  }
+})
+
+
+function goToSection(i){
+  $("fieldset:gt("+i+")").removeClass("current").addClass("next");
+  $("fieldset:lt("+i+")").removeClass("current");
+  $("li").eq(i).addClass("current").siblings().removeClass("current");
+  setTimeout(function(){
+    $("fieldset").eq(i).removeClass("next").addClass("current active");
+      if ($("fieldset.current").index() == 3){
+        $("#next").hide();
+        $("input[type=submit]").show();
+      } else {
+        $("#next").show();
+        $("input[type=submit]").hide();
+      }
+  }, 80);
+
+}
+  
+  
+jQuery.validator.addMethod("phoneUS", function(phone_number, element) {
+    phone_number = phone_number.replace(/\s+/g, ""); 
+	return this.optional(element) || phone_number.length > 9 &&
+		phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
+}, "Please specify a valid phone number");
